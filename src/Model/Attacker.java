0ba6cf06dacc923a4a11a17c233fbd2474febd90
CaptainTaihu <emanns95@gmail.com>
2014-11-10 13:01:49 -0700

    Added test comment

--- src/Model/Attacker.java
+++ src/Model/Attacker.java
@@ -1,56 +1,56 @@
 package Model;
 
+//Test, let's see if git is working.
 public abstract class Attacker {
 
 	private int hitpoints;
 	private int attackRating;
 	private int range;
-	private int speed;		//The number of milliseconds it takes to cross a full tile
-	private int defenseRating;  
+	private int speed; // The number of milliseconds it takes to cross a full
+						// tile
+	private int defenseRating;
 
-	//private int cost; //This is for itr2, possibly
-	
+	// private int cost; //This is for itr2, possibly
+
 	public Attacker(int hp, int def, int ar, int range, int spd) {
-		
+
 		hitpoints = hp;
 		attackRating = ar;
 		this.range = range;
 		speed = spd;
 		defenseRating = def;
-		
+
 	}
-	
-	
 
 	public abstract void move();
+
 	public abstract void attack(Structure s);
+
 	public abstract void die();
-	
+
 	public void takeDamage(int dmg) {
 		hitpoints -= dmg;
 	}
 
-	//getters
+	// getters
 	public int getHP() {
 		return hitpoints;
 	}
-	
+
 	public int getAttack() {
 		return attackRating;
 	}
+
 	public int getRange() {
 		return range;
 	}
+
 	public int getSpeed() {
 		return speed;
 	}
+
 	public int getAC() {
 		return defenseRating;
 	}
-	
-	
-	
-	
-	
-	
+
 }
