34739839a77d1ffb91affa9ed5d56f9c4122e644
CaptainTaihu <emanns95@gmail.com>
2014-11-10 12:47:36 -0700

    Testing

--- src/Maps/DesertUprising.java
+++ src/Maps/DesertUprising.java
@@ -11,10 +11,10 @@ import Model.Map;
 import Structures.BaseDesertUprising;
 
 public class DesertUprising extends Map {
-	
+
 	private static int height = 30;
 	private static int width = 40;
-	
+
 	public DesertUprising() {
 		super(height, width);
 		setPath();
@@ -26,35 +26,35 @@ public class DesertUprising extends Map {
 
 	private void setTiles() {
 		// TODO Auto-generated method stub. PLACE ANY OTHER TILE CHANGES HERE
-		
+
 	}
 
 	@Override
 	public void setBase() {
-		//For now we'll set the base in the left-middle tile (0,15)
+		// For now we'll set the base in the left-middle tile (0,15)
 		gameBoard.get(0).get(15).build(new BaseDesertUprising());
-		
+
 	}
 
 	@Override
 	public void setSpawnPoints() {
-		//One spawn point on opposite end of the board
-		gameBoard.get(width-1).get(15).setSpawn(true);
-		
+		// One spawn point on opposite end of the board
+		gameBoard.get(width - 1).get(15).setSpawn(true);
+
 	}
 
 	@Override
 	public void setPath() {
-		for(int i = 1; i<width; i++) {
+		for (int i = 1; i < width; i++) {
 			gameBoard.get(i).get(15).setMove(true);
 		}
-		
+
 	}
 
 	@Override
 	public void setStartingResources() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
