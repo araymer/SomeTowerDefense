b4b603c49a9cb744f9f3aa44cf0b86c6ec1694e2
dougwade520 <dougwade520@gmail.com>
2014-11-10 13:19:35 -0700

    Added enum Dir, private inner class in Structure

--- src/Model/Structure.java
+++ src/Model/Structure.java
@@ -58,45 +58,46 @@ public abstract class Structure {
 	public SpecialAttack getSpecial() {
 		return special;
 	}
-}
 
-/**
- * This class is attached to attackers and defenders to determine if a hostile
- * unit is in range and to initiate hostile activity against them.
- * 
- * @author Team Something
- *
- */
-private class Detector {
-	int x, y, range; // class attributes
-	Map map;
-
-	public Detector(int locX, int locY, Map theMap, int theRange) {
-		x = locX;
-		y = locY;
-		map = theMap;
-		range = theRange;
-
-	}
 
 	/**
-	 * Checks if any objects that can be attacked is within range.
+	 * This class is attached to attackers and defenders to determine if a hostile
+	 * unit is in range and to initiate hostile activity against them.
 	 * 
-	 * @return if something is in range and can be attacked
+	 * @author Team Something
+	 *
 	 */
-	public boolean scan() {
-		for(int a = 0; a < MasterList.unitList.toArray().length; a++){
+	private class Detector {
+		int x, y, range; // class attributes
+		Map map;
+
+		public Detector(int locX, int locY, Map theMap, int theRange) {
+			x = locX;
+			y = locY;
+			map = theMap;
+			range = theRange;
+
+		}
+
+		/**
+		 * Checks if any objects that can be attacked is within range.
+		 * 
+		 * @return if something is in range and can be attacked
+		 */
+		public boolean scan() {
+			for(int a = 0; a < MasterList.unitList.toArray().length; a++){
 			
-			if(Math.abs((x - MasterList.unitList[a].toArray().length) <= range) && 
+				if(Math.abs((x - MasterList.unitList[a].toArray().length) <= range) && 
 					(Math.abs(y - MasterList.unitList[a].toArray().length) <= range) 
 					&& MasterList.unitList[a].hostile()) <= range {
 				
+				}
 			}
+			return false;
 		}
-		return false;
-	}
 
-	public void shoot(Object O) {
+		public void shoot(Object O) {
 
+		}
 	}
 }
