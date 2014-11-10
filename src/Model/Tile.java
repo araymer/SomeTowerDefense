6e58421fc8db8fbcb6697952a4424b96988e8625
CaptainTaihu <emanns95@gmail.com>
2014-11-10 13:20:21 -0700

    Set so tile can determine where units should move. getDirection and setDirection

--- src/Model/Tile.java
+++ src/Model/Tile.java
@@ -9,58 +9,69 @@ public class Tile {
 	private boolean movable;
 	private Vector<Attacker> attackers;
 	private Structure tileStructure;
-	
+	private Dir nextTile;
+
 	public Tile() {
-		
+
 		spawnable = false;
 		buildable = false;
 		movable = false;
 		tileStructure = null;
 		attackers = new Vector<Attacker>();
-		
+
 	}
-	
-	
+
 	public boolean getSpawn() {
 		return spawnable;
-		
+
 	}
+
 	public boolean getBuild() {
 		return buildable;
-		
+
 	}
+
 	public boolean getMove() {
 		return movable;
-		
+
 	}
-	
+
+	public Dir getDirection() {
+		if (movable) {
+			return nextTile;
+		}
+	}
+
 	public void setSpawn(boolean b) {
 		spawnable = b;
-		
+
 	}
+
 	public void setBuild(boolean b) {
 		buildable = b;
-		
+
 	}
+
 	public void setMove(boolean b) {
 		movable = b;
-		
+
 	}
-	
+
+	public void setDirection(Dir direction) {
+		nextTile = direction;
+	}
+
 	public void addAttacker(Attacker a) {
 		attackers.add(a);
 	}
+
 	public boolean build(Structure s) {
-		if(buildable && tileStructure == null) {
+		if (buildable && tileStructure == null) {
 			tileStructure = s;
-		return true;
-		}
-		else
+			return true;
+		} else
 			return false;
-		
+
 	}
-	
-	
-	
-	
+
 }
