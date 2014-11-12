3b1c2c748192a7298438667823fbb49d17b73f95
araymer <aaronraymer26@gmail.com>
2014-11-11 19:15:17 -0700

    .

--- src/Maps/DesertUprising.java
+++ src/Maps/DesertUprising.java
@@ -1,23 +1,21 @@
 /*
- *GIT IS WORKING IF YOU CAN READ THIS 
- 
-
-* 
-* A desert marine outpost world. Conditions here are rough and supplies are sparse. The marines
+ * 
+ * A desert marine outpost world. Conditions here are rough and supplies are sparse. The marines
  * want off this gods-forsaken hellhole and have taken up arms in mutiny!
  * 
  */
 
 package Maps;
 
+import Model.Dir;
 import Model.Map;
 import Structures.BaseDesertUprising;
 
 public class DesertUprising extends Map {
-	
+
 	private static int height = 30;
 	private static int width = 40;
-	
+
 	public DesertUprising() {
 		super(height, width);
 		setPath();
@@ -28,36 +26,38 @@ public class DesertUprising extends Map {
 	}
 
 	private void setTiles() {
-		// TODO Auto-generated method stub. PLACE ANY OTHER TILE CHANGES HERE
-		
+		// PLACE ANY OTHER TILE CHANGES HERE
+
 	}
 
 	@Override
 	public void setBase() {
-		//For now we'll set the base in the left-middle tile (0,15)
-		gameBoard.get(0).get(15).build(new BaseDesertUprising());
-		
+		// For now we'll set the base in the left-middle tile (0,15)
+		gameBoard.get(0).get(15).build(new BaseDesertUprising(0, 15));
+
 	}
 
 	@Override
 	public void setSpawnPoints() {
-		//One spawn point on opposite end of the board
-		gameBoard.get(width-1).get(15).setSpawn(true);
-		
+		// One spawn point on opposite end of the board
+		gameBoard.get(width - 1).get(15).setSpawn(true);
+
 	}
 
 	@Override
 	public void setPath() {
-		for(int i = 1; i<width; i++) {
+		for (int i = 1; i < width; i++) {
 			gameBoard.get(i).get(15).setMove(true);
+			gameBoard.get(i).get(15).setDirection(Dir.LEFT);
+			// in this case very simple because jsut one direction
 		}
-		
+
 	}
 
 	@Override
 	public void setStartingResources() {
-		// TODO Auto-generated method stub
-		
+		// TODO set starting resources for the player
+
 	}
 
 }
