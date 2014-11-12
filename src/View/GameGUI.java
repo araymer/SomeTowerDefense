5dd387c715e84262f6174bd4a5f2553128ad6973
marcosgomez <marcosgomez@email.arizona.edu>
2014-11-12 08:22:24 -0700

    Created basic GUI with Map and Tile Panel

---
+++ src/View/GameGUI.java
@@ -0,0 +1,52 @@
+package View;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.io.Serializable;
+
+import javax.swing.JFrame;
+
+/**
+ * The class that organizes all the GUI elements for the tower defense game
+ * 
+ * @author Marcos
+ *
+ */
+public class GameGUI implements Serializable{
+	
+	private int FRAME_WIDTH = 800;
+	//desertuprising.jpg seems to have height 622
+	private int FRAME_HEIGHT = 600;
+	JFrame frame;
+	
+	public static void main(String[] args) {
+		GameGUI gui = new GameGUI();
+	}
+	
+	/**
+	 * Constructs the Tower Defense GUI
+	 */
+	public GameGUI(){
+		createFrame();
+		
+		MapPanel mapPanel = new MapPanel("desertuprising.jpg");
+		frame.add(mapPanel);
+		mapPanel.add(new TilePanel());
+		frame.setVisible(true);
+	}
+	
+	/**
+	 * Creates and sets the JFrame
+	 */
+	private void createFrame() {
+		frame = new JFrame();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(FRAME_WIDTH, FRAME_HEIGHT);
+		//Color is for debug purposes
+		frame.setBackground(Color.GREEN);
+		frame.getContentPane().setBackground(Color.MAGENTA);
+		frame.setTitle("Some Tower Defense");
+
+	}
+
+}
