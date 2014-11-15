6c7f641e7fde36b5cac2a57a028fd8245cbc803d
araymer <aaronraymer26@gmail.com>
2014-11-15 13:54:08 -0700

    Prepping for master game loop

---
+++ src/Model/Ticker.java
@@ -0,0 +1,10 @@
+package Model;
+
+public class Ticker implements Runnable {
+
+	@Override
+	public void run() {
+		//implement our ticker code (update EVERYTHING and prepare for the next paint)
+	}
+	
+}
