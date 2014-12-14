fe289a21d875841e7bcd4a55756aa551a92bed2c
marcosgomez <marcosgomez@email.arizona.edu>
2014-12-14 14:56:36 -0700

    added stuff to allow persistence

--- src/View/GameGUI.java
+++ src/View/GameGUI.java
@@ -27,6 +27,7 @@ import Controller.GameController;
 import Controller.TDClient;
 import Model.Attacker;
 import Model.Map;
+import Model.Player;
 import Model.Structure;
 import Model.Ticker;
 import Model.Tile;
@@ -146,10 +147,15 @@ public class GameGUI implements Serializable {
 
 	}
 	
-	public void loadMap(Map map){
+	public void createLoadedMap(Map map){
 		MapPanel.getInstance().setMap(map.mapImageName);
+		
+		Player.getInstance().setMoney(map.playerMoney.getMoney());
+		
 		tilePanel = TilePanel.getInstance();
-		//tilePanel.setMap(gameMap);
+		tilePanel.setMap(map);
+		
+		
 
 		MouseListener placementListener = new PlacementListener();
 
