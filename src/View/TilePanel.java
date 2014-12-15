a95c6868bfee63688dc33ba8469ce1a0ee0f5e26
marcosgomez <marcosgomez@email.arizona.edu>
2014-12-14 19:48:35 -0700

    centered range circle more

--- src/View/TilePanel.java
+++ src/View/TilePanel.java
@@ -133,7 +133,7 @@ public class TilePanel extends JPanel implements Serializable {
 
 		if (circleX != -1 && circleY != -1) {
 			g2.setColor(new Color(1, 1, 1, 0.2f));
-			g2.fillOval(circleX - range / 4, circleY - range / 4, range, range);
+			g2.fillOval(circleX - range / 3, circleY - range / 3, range, range);
 		}
 	}
 
